f8adbdf775630fdf686d0f0c4947944a3ab15b50
Alex Ortiz <afo5147@psu.edu>
2026-03-22 21:43:28 -0400

    Added playlist class and fixxed some problems

--- playlist.py
+++ playlist.py
@@ -11,3 +11,33 @@ Design notes:
   • __str__ uses a generator expression with str.join() for a concise
     multi-line string without building an intermediate list manually.
 """
+from music_track import MusicTrack
+
+class Playlist:
+    '''
+    This is going to manage all of the tracks in the playlist.
+    '''
+    def __init__(self):
+        self._tracks = []
+
+    def add_track(self, track: MusicTrack) -> None:
+        # Add a track to the playlist.
+        self._tracks.append(track)
+
+    def clear_playlist(self) -> None:
+        # Clear the playlist and getting rid of all the tracks.
+        self._tracks.clear()
+
+    def sort_by_release_year(self) -> None:
+        # This is going to sort the tracks by release year.
+        self._tracks.sort()
+
+  
+    @property
+    def tracks(self) -> list[MusicTrack]:
+        # This is going to get a copy of the trasks list.
+        return list(self._tracks)
+    
+
+    def __str__(self):
+        return "\n".join(str(track) for track in self._tracks)
