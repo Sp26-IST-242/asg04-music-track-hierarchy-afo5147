d4965f476980fccd661ab106d4ef8b86b235c748
Alex Ortiz <afo5147@psu.edu>
2026-03-22 20:17:33 -0400

    Added method for getting artist info

--- artist.py
+++ artist.py
@@ -6,3 +6,21 @@ It introduces two core Python OOP conventions:
   1. The single leading-underscore (_name) signals a non-public attribute.
   2. @property exposes a clean public getter without allowing direct mutation.
 """
+class Artist:
+    def __init__(self, name, genre):
+        self._name = name
+        self._genre = genre
+
+    @property
+    def name(self):
+        # this is to get the name of the artist
+        return self._name
+
+    @property
+    def genre(self):
+        # this is to get the genre of the artist
+        return self._genre
+    
+    def __str__(self):
+        # This is going to be the format for what is returned when printed
+        return f"{self._name} ({self._genre})"
