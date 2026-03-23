7c61854fda068a1a9ae65131016b7fe37493a40a
Alex Ortiz <afo5147@psu.edu>
2026-03-22 20:43:25 -0400

    Added the album class and its properties/methods.

--- album.py
+++ album.py
@@ -8,3 +8,33 @@ Key concepts to implement:
   • A *derived* property (debut_year) that computes its value from stored data
     rather than keeping a second field in sync.
 """
+class Album:
+    
+    def __init__(self, title: str, active: bool, years: list[int]):
+        # This is going to make sure that the list is not empty.
+        if not years:
+            raise ValueError("Please make sure years list is not empty.")
+        
+        self._title = title
+        self._active = active
+        self._years = list(years) # This is going to make sure that only a copy is stored
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def active(self) -> bool:
+        return self._active
+
+    @property
+    def years(self) -> list[int]:
+        return list(self._years)
+
+    @property
+    def debut_year(self) -> str:
+        # This is going to take the minimum of the years list and make it the debut year.
+        return self._years[0]
+
+    def __str__(self):
+        return f"{self._title} active = {self._active} debut year: {self.debut_year}"
